ecc554da3f73bf9f53442ca453d43c375bf4767b
charlie-murray <cm967@exeter.ac.uk>
2022-06-14 04:27:58 +0100

    Done quite a lot. Added my own system of working out the cointergration, added a way of finding the best pairs od stocks to trade with. Haven't finished working out the ratios, next to finsh working out the ratios then working out the upper and lower bounds for trading signals

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Python-Trading
 
 This is a python programing project over the summer of 2022
+
+ALSO SAVING PARTIAL BITS OF DATA IN HERE I MIGHT NEED LATER
+
+start = '2020-12-31'
+    end = '2022-03-08'
+    fee = 0.001
+    window = 252
+    t_threshold = -2.5
+    
+    #start = dt(2017, 1, 1)
+    symbols_list = ['AAPL', 'F', 'TWTR', 'FB', 'AAL', 'AMZN', 'GOOGL', 'GE']
+    
+    symbols=[]
+
+    #pull price using iex for each symbol in list defined above
+    for ticker in symbols_list: 
+        r = yf.download(ticker, start, end)
+        # add a symbol column
+        r['Symbol'] = ticker 
+        symbols.append(r)
+
+    # concatenate into df
+    df = pd.concat(symbols)
+    df = df.reset_index()
+    df = df[['Date', 'Close', 'Symbol']]
+    
+    #have to pivot the df
+    df_pivot = df.pivot('Date','Symbol','Close').reset_index()
+    df_pivot.head()
+    
+    corr_df = df_pivot.corr(method='pearson')
+    #reset symbol as index (rather than 0-X)
+    corr_df.head().reset_index()
+    
+    print(corr_df)
--- pairAlgorithm.py
+++ pairAlgorithm.py
@@ -1,4 +1,6 @@
+from ctypes import resize
 import datetime as dt
+from unittest import result
 import searchStock as ss
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +8,9 @@ import numpy as np
 import statsmodels.api as sm
 import yfinance as yf
 import scipy.optimize as spop
+import pandas_datareader as web
+import csv
+from statsmodels.tsa.stattools import adfuller
 
 def plot_chart(value, dates, symbols):
     
@@ -46,78 +51,100 @@ def get_last_year(symbol):
     year_value = value[-252:]
     return year_value, year_dates
 
-def cointegrated_pairs(stocks):
-    start = '2019-12-31'
-    end = '2021-03-08'
-    fee = 0.001
-    window = 252
-    t_threshold = -2.5
+def cointegrated_calculator(file):
+    start = '2020-12-31'
+    end = '2022-03-08'
+    
+    all_stocks = []
+    
+    with open(file, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        i = 0
+        for row in spamreader:
+            if i != 0:
+                strSplit = row[0].split(',')
+                all_stocks.append(strSplit[0])
+            i += 1 
     
     data = pd.DataFrame()
-    returns = pd.DataFrame()
     
-    for stock in stocks:
+    for stock in all_stocks:
         prices = yf.download(stock, start, end)
         data[stock] = prices['Close']
-        returns[stock] = np.append(data[stock][1:].reset_index(drop=True)/data[stock][:-1].reset_index(drop=True) - 1, 0)
     
-    gross_returns = np.array([])
-    net_returns = np.array([])
-    t_s = np.array([])
-    stock1 = stocks[0]
-    stock2 = stocks[1]
+    p_values = pd.DataFrame()
     
-    for t in range(window, len(data)):
+    for i in data:
+        p_col = []
+        for j in data:
+            if i != j:
+                result = adfuller(data[i] - data[j])
+                p_col.append(result[1])
+            else:
+                p_col.append(0.0)
+        p_values[i] = p_col
+    
+    p_values.to_csv('p_value.csv', index = False, header=True)
+
+def calculate_signals(stocks):
+    
+    start = '2020-12-31'
+    end = '2022-03-08'
+    
+    strSplit = stocks.split(',')
+    stock = [strSplit[0], strSplit[1]]
+    p_value = strSplit[2]
+    
+    stock_frame = pd.DataFrame()
+    
+    for s in stock:
+        prices = yf.download(s, start, end)
+        stock_frame[s] = prices['Close']
         
-        def unit_root(b):
-            a = np.average(data[stock2][t-window:t] - b*data[stock1][t-window:t])
-            fair_value = a + b*data[stock1][t-window:t]
-            diff = np.array(fair_value - data[stock2][t-window:t])
-            diff_diff = diff[1:] - diff[:-1]
-            reg = sm.OLS(diff_diff, diff[:-1])
-            res = reg.fit()
-            return res.params[0]/res.bse[0]
-        
-        res1 = spop.minimize(unit_root, data[stock2][t]/data[stock1][t], method='Nelder-Mead')
-        t_opt = res1.fun
-        b_opt = float(res1.x)
-        a_opt = np.average(data[stock2][t-window:t] - b_opt*data[stock1][t-window:t])
-        fair_value = a_opt + b_opt*data[stock1][t]
-        
-        if t == window:
-            old_signal = 0
-        if t_opt > t_threshold:
-            signal = 0
-            gross_returns = 0
-        else:
-            signal = np.sign(fair_value - data[stock2][t])
-            gross_returns = signal*returns[stock2][t] - signal*returns[stock1][t]
-        
-        fees = fee*abs(signal - old_signal)
-        net_returns = gross_returns - fees
-        gross_returns = np.append(gross_returns, gross_returns)
-        net_returns = np.append(net_returns, net_returns)
-        t_s = np.append(t_s, t_opt)
-        
-        print('day ' + str(data.index[t]))
-        print('')
-        if signal == 0:
-            print('no trading')
-        elif signal == 1:
-            print('long position on ' + stock2 + ' and short position on ' + stock1)
-        else:
-            print('long position on ' + stock1 + ' and short position on ' + stock2)
-        print('gross daily returns: ' + str(np.round(gross_returns*100, 2)) + '%')
-        print('net daily returns: ' + str(np.round(net_returns*100, 2)) + '%')
-        print('cumulative net returns so far: ' + str(np.round(np.prod(1+net_returns)*100-100, 2)) + '%')
-        
-        #plt.plot(np.append(np.cumprod(1+gross_returns)))
-        #plt.plot(np.append(np.cumprod(1+net_returns)))
-        
-        plt.show()
-        
+    ratio_stock = pd.DataFrame()
+
+    for i in range(0, len(stock_frame.index)):
+        stock_values = []
+        for symbol in stock_frame:
+            stock_values.append(stock_frame.loc[i][symbol])
+        ratio_stock.append({'ratios':stock_values[0]/stock_values[1]})
+    
+    plt.plot(ratio_stock)
+    plt.show()
+
+    
+def calculate_pairs(file):
+    
+    p_values = pd.read_csv(file)
+    
+    tradeable = []
+    
+    for stock in p_values:
+        stock_pval = p_values[stock]
+        for p in stock_pval:
+            if p != 0.0 and p < 0.05:
+                stock_row = p_values[p_values[stock] == p].index[0]
+                y_stock = p_values.columns[stock_row]
+                test_stock = y_stock + "," + stock
+                if test_stock not in tradeable:
+                    tradeable.append(stock + "," + y_stock + "," + str(p))
+    
+    best_pair(tradeable)
+
+def best_pair(stocks):
+    
+    best_stock = 'x,y,0.05'
+    
+    for stock in stocks:
+        option1 = stock.split(',')
+        option2 = best_stock.split(',')
+        if float(option1[2]) < float(option2[2]):
+            best_stock = stock
+    
+    calculate_signals(best_stock)
+
 if __name__ == '__main__':
-    vals = ['AAPL', 'AAL']
-    cointegrated_pairs(vals)
+    #cointegrated_calculator('list_of_stocks.csv')
     #plot_chart()
     #run_graph(vals, 'y')
+    calculate_pairs('p_value.csv')
